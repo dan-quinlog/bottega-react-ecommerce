12fe74d1333318ea2feddfeead193606e26164b5
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-17 12:33:30 -0600

    Built out header and nav structure and styles
    Built out header and nav structure and set default styles *beforeafter

---
+++ src/components/headernavbar/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className='navbar'>
+
+      </div>
+    );
+  }
+}
+
+export default Navbar;
