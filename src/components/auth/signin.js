902fe25c25ae5b0c483bf77c83ec5f925d82b013
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-17 14:29:56 -0600

    built sing in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className='sign-in'>
+        sign in
+      </div>
+    );
+  }
+}
+
+export default SignIn;
