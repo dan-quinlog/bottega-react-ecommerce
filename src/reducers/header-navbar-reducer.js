74cacf281a7714466bf28720cde8fe228e60e511
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-23 16:34:50 -0600

    built base structure for account components

--- src/reducers/header-navbar-reducer.js
+++ src/reducers/header-navbar-reducer.js
@@ -1,24 +1,8 @@
 import { SET_HEADER_LINKS, SET_NAVBAR_LINKS } from "../actions/types";
 
 const INITIAL_STATE = {
-  headerLinks: [
-    {
-      title: "account"
-    },
-    {
-      title: "purchase"
-    }
-  ],
-  navbarLinks: [
-    {
-      title: "account",
-      active: true
-    },
-    {
-      title: "purchase",
-      active: false
-    }
-  ]
+  headerLinks: [],
+  navbarLinks: []
 };
 
 export default function(state = INITIAL_STATE, action) {
