4567c850c6e3542261e1da75c2e60f52356b9bf1
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-24 11:08:17 -0600

    finished navbar feature

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class Navbar extends Component {
   constructor() {
@@ -12,9 +13,9 @@ class Navbar extends Component {
         {this.props.navbarLinks.map((link, index) => {
           return (
             <a
-              className={`navbar__link ${link.active ? 'green-text' : ''}`}
+              className={`navbar__link ${link.active ? "green-text" : ""}`}
               key={index}
-              onClick={() => console.log("trying to switch tab")}
+              onClick={() => this.props.changeNavbarActive(link._id)}
             >
               {link.title}
             </a>
@@ -32,6 +33,6 @@ function mapStateToProps(state) {
   };
 }
 
-Navbar = connect(mapStateToProps)(Navbar);
+Navbar = connect(mapStateToProps, actions)(Navbar);
 
 export default Navbar;
