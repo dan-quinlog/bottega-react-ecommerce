80ea61d6b21b84fd7d35c82fb7f4eb73b338ec97
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-24 13:12:21 -0600

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,91 @@ export function fetchUserPurchases() {
     payload: [
       {
         _id: 0,
-        title: 'purchase 1',
-        amount: '8.02'
+        total: '8.02',
+        orderNumber: '109123dj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
       },
       {
         _id: 1,
-        title: 'purchase 2',
-        amount: '18.02'
+        total: '84.02',
+        orderNumber: '104w56hwe465h',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 2,
+        total: '84.02',
+        orderNumber: '1024gf23dj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 3,
+        total: '18.40',
+        orderNumber: '1f042f24dj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 4,
+        total: '48.02',
+        orderNumber: '1032q4g3qaj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 5,
+        total: '132.02',
+        orderNumber: '10945gw35gj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 6,
+        total: '38.02',
+        orderNumber: '1243f23dj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
+      },
+      {
+        _id: 7,
+        total: '28.02',
+        orderNumber: '12353dj',
+        orderDate: new Date(),
+        creditCard: '-0000',
+        user: {
+          name: 'Jordan Hudgens',
+          shippingAddress: '1234 West State',
+        }
       },
     ]
   })
