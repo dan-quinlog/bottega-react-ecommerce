db1d72d77995bec58f561979397e0a539b60986c
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-26 13:56:01 -0600

    completed account information form

--- src/components/account/account-information-form.js
+++ src/components/account/account-information-form.js
@@ -70,6 +70,7 @@ class AccountInformationForm extends Component {
           name="zipcode"
           component={FormInput}
         />
+        <div className="account-information-form__line"></div>
 
         {this.state.showPasswords ? (
           [
@@ -99,6 +100,25 @@ class AccountInformationForm extends Component {
               placeholder="Confirm Password"
               name="confirm"
               component={FormInput}
+            />,
+            <Field
+              key={4}
+              className="account-information-form__update-information"
+              onClick={() => History.push("/account")}
+              type="submit"
+              title="Update Information"
+              name="update-information"
+              component={FormButton}
+            />,
+            <Field
+              key={5}
+              className="account-information-form__cancel"
+              onClick={() => this.setState({ showPasswords: false })}
+              type="button"
+              title="Cancel"
+              name="cancel"
+              short={true}
+              component={FormButton}
             />
           ]
         ) : (
