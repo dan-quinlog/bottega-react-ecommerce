0d91522364bfe81e9c72e5058b7f7bde50a7493c
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-30 16:05:31 -0600

    built review form details

--- src/components/order/review-form.js
+++ src/components/order/review-form.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
-import History from '../../history';
+import History from "../../history";
 
 import { FormButton } from "../form-fields";
-import ReviewProducts from './review-products';
+import ReviewProducts from "./review-products";
 
 class ReviewForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
-    
+
     return (
       <form onSubmit={handleSubmit} className={`${className} review-form`}>
-        <div className='review-form__legend'>
-          <div className='review-form__legend__name'>Name</div>
-          <div className='review-form__legend__quantity'>Quantity</div>
-          <div className='review-form__legend__price'>Price</div>
+        <div className="review-form__legend">
+          <div className="review-form__legend__name">Name</div>
+          <div className="review-form__legend__quantity">Quantity</div>
+          <div className="review-form__legend__price">Price</div>
         </div>
-        <ReviewProducts className='review-form__products'/>        
+        <ReviewProducts className="review-form__products" />
         <div className="review-form__line"></div>
         <Field
           className="review-form__proceed"
-          onClick={ () => History.push('/signin')}
+          onClick={() => History.push("/signin")}
           type="submit"
           title="Proceed to Checkout"
           name="proceed"
@@ -29,13 +29,35 @@ class ReviewForm extends Component {
         />
         <Field
           className="review-form__back"
-          onClick={ () => History.push('/shop')}
+          onClick={() => History.push("/shop")}
           type="button"
           title="Back"
           name="back"
           short={true}
           component={FormButton}
         />
+        <div className="review-form__details review-details">
+          <div className="review-details__subtotal review-detail">
+            <div className="review-detail__title">Subtotal</div>
+            <div className="review-detail__price">$7.96</div>
+          </div>
+          <div className="review-details__tax review-detail">
+            <div className="review-detail__title">Tax</div>
+            <div className="review-detail__price">$0.96</div>
+          </div>
+          <div className="review-details__shipping review-detail">
+            <div className="review-detail__title">Shipping</div>
+            <div className="review-detail__price">$0.00</div>
+          </div>
+          <div className="review-details__total review-detail review-detail-green">
+            <div className="review-detail__title review-detail-green__title">
+              Total
+            </div>
+            <div className="review-detail__price review-detail-green__price">
+              $8.12
+            </div>
+          </div>
+        </div>
       </form>
     );
   }
