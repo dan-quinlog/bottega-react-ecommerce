840d442fe20c88148b04d6004c21bcca70dd3b8d
Tombo Yuuko <dan.quinlog@gmail.com>
2019-10-31 11:10:36 -0600

    built payment form

--- src/components/information/payment-form.js
+++ src/components/information/payment-form.js
@@ -4,88 +4,65 @@ import { reduxForm, Field } from "redux-form";
 
 import { FormInput, FormButton } from "../form-fields";
 import History from "../../history";
+import OrderSummary from './order-summary';
 
 class PaymentForm extends Component {
   render() {
     const { className, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
+      <form onSubmit={handleSubmit} className={`${className} payment-form`}>
         <Field
-          className="sign-up-form__name"
+          className="payment-form__name"
           type="name"
-          title="Name"
+          title="Name on Credit Card"
           placeholder="Name"
           name="name"
           component={FormInput}
         />
         <Field
-          className="sign-up-form__street-address"
-          type="address"
-          title="Street Address"
-          placeholder="address"
-          name="address"
+          className="payment-form__card"
+          type="card"
+          title="Credit Card Number"
+          placeholder="____-____-____-____"
+          name="card"
           component={FormInput}
         />
         <Field
-          className="sign-up-form__city"
-          type="city"
-          title="City"
-          placeholder="City"
-          name="city"
+          className="payment-form__expiration"
+          type="expiration"
+          title="Expiration Date"
+          placeholder="expiration"
+          name="expiration"
           component={FormInput}
         />
         <Field
-          className="sign-up-form__state"
-          type="state"
-          title="State"
-          placeholder="State"
-          name="state"
+          className="payment-form__ccv"
+          type="CCV"
+          title="CCV"
+          placeholder="CCV"
+          name="CCV"
           component={FormInput}
         />
+        <div className="payment-form__line"></div>
         <Field
-          className="sign-up-form__zipcode"
-          type="zipcode"
-          title="Zipcode"
-          placeholder="Zipcode"
-          name="zipcode"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__password"
-          type="password"
-          title="Password"
-          placeholder="Password"
-          name="password"
-          component={FormInput}
-        />
-        <Field
-          className="sign-up-form__confirm"
-          type="password"
-          title="Confirm Password"
-          placeholder="Confirm Password"
-          name="confirm"
-          component={FormInput}
-        />
-
-        <div className="sign-up-form__line"></div>
-        <Field
-          className="sign-up-form__use-this-address"
+          className="payment-form__pay-complete"
           onClick={() => History.push("/information/payment")}
           type="submit"
-          title="Use This Address"
-          name="use-this-address"
+          title="Complete"
+          name="pay-complete"
           component={FormButton}
         />
         <Field
-          className="sign-up-form__back"
-          onClick={() => History.push("/signin")}
+          className="payment-form__back"
+          onClick={() => History.push("/shop")}
           type="button"
           title="Back"
           name="back"
           short={true}
           component={FormButton}
         />
+        <OrderSummary className='payment-form__order-summary' />
       </form>
     );
   }
--- src/components/information/shipping-form.js
+++ src/components/information/shipping-form.js
@@ -72,7 +72,7 @@ class ShippingForm extends Component {
         <div className="shipping-form__line"></div>
         <Field
           className="shipping-form__use-this-address"
-          onClick={() => History.push("/payment")}
+          onClick={() => History.push("/information/payment")}
           type="submit"
           title="Use This Address"
           name="use-this-address"
